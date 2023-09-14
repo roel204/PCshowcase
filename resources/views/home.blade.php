3d36d933a3571ebeab1cf63afaac4ee23236b6b8
Roel-H <roelhoogendoorn01369@gmail.com>
2023-09-14 15:54:06 +0200

    Added layout & partials

---
+++ resources/views/home.blade.php
@@ -0,0 +1,8 @@
+@extends('layout.web')
+
+@section('title', 'Home')
+
+@section('content')
+    <h1>Welcome to the Home Page</h1>
+    <p>This is the content of the home page.</p>
+@endsection
---
+++ resources/views/partials/nav.blade.php
@@ -0,0 +1,5 @@
+<div class="navbar">
+    <a href="/">Home</a>
+    <a href="/about">About</a>
+    <a href="/contact">Contact</a>
+</div>
