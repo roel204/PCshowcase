249b03727b495b7b4a264e446813f6e47406c1b0
Roel-H <roelhoogendoorn01369@gmail.com>
2023-09-29 11:50:43 +0200

    Removed some code

--- app/Http/Controllers/ComputerController.php
+++ app/Http/Controllers/ComputerController.php
@@ -7,8 +7,6 @@ use Illuminate\Http\Request;
 class ComputerController extends Controller
 {
     function index () {
-        $computer = new Computer();
-        $computer->title = 'MainPC';
-        $computer->price = 700;
+
     }
 }
